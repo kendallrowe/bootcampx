fa31c2034c9b54e4de60ae33d8d575558af3dc1c
Kendall Rowe <kendall.rowe312gmail.com>
2019-07-24 18:25:19 +0000

    Parameterized queries added to teachers and students

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -11,9 +11,9 @@ pool.query(`
   SELECT students.id as student_id, students.name as name, cohorts.name as cohort
   FROM students
   JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '%${process.argv[2]}%'
-  LIMIT ${process.argv[3] || 5};
-`)
+  WHERE cohorts.name = $1
+  LIMIT $2;
+`, [process.argv[2], process.argv[3] || 5])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
